967779dcb27cd27af7742178bba6ce0ad324dc1d
pinnarbolu-ctrl <pinnarbolu@gmail.com>
2026-08-05 13:05:01 +0300

    Update telegram_notifier.py
    token eklendi

--- telegram/telegram_notifier.py
+++ telegram/telegram_notifier.py
@@ -17,8 +17,8 @@ class TelegramNotifier:
         Telegram bilgilerini ortam değişkenlerinden okur.
 
         Gerekli değişkenler:
-        TELEGRAM_BOT_TOKEN
-        TELEGRAM_CHAT_ID
+        BOT_TOKEN = "8855467313:AAHYdR1ts-liJ0hMwxxPGpgmrPne6ydFOpI"
+        CHAT_IDS = [2097448038]
         """
 
         self.bot_token = os.getenv(
